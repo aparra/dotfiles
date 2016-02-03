4f1aa22755830de4ec50f7bbfae754cc7eb2ccfb
Anderson Parra de Paula <anderson.parradepaula@soundcloud.com>
2016-02-03 12:03:25 +0100

    add alias to fix vpn connection at default docker container

--- profile.d/alias.sh
+++ profile.d/alias.sh
@@ -6,3 +6,4 @@ alias envconfig='gvim ~/env.sh'
 alias sbt_eclipse="sbt 'eclipse with-source=true'"
 alias plantuml='java -jar ~/.m2/repository/net/sourceforge/plantuml/plantuml/8018/plantuml-8018.jar'
 alias ws='cd ~/development/workspace' # ws => workspace
+alias fix_vpn='docker-machine scp /etc/resolv.conf default:/etc/resolv.conf'
