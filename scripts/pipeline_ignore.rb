a104472e3c0cb5ad33b80731b45ebaac6e7308fc
Anderson Parra de Paula <anderson.parradepaula@soundcloud.com>
2016-05-31 16:00:55 +0200

    add pipeline ignore

---
+++ scripts/pipeline_ignore.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+LAST_FILTER='<ignore pattern="*" />'
+project_name = ARGV[0]
+
+puts "Ignoring changes for #{project_name}"
+
+eligible_pipelines = Dir.glob("*/pipeline.xml")
+pipelines = eligible_pipelines.reject {|f| f.start_with?("nielsen") }
+
+pipelines.each do |file_name|
+  puts "* #{file_name}"
+  content = File.read(file_name)
+  content[LAST_FILTER] = 
+    "<ignore pattern=\"#{project_name}/**/*\" />" + "\n" + LAST_FILTER.rjust(30, ' ')
+  File.open(file_name, "w") {|f| f.puts content }
+end
+
