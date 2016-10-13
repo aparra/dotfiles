2e0825867b1a8f269701cb601c563c10a5177bd5
Anderson Parra de Paula <anderson.parradepaula@soundcloud.com>
2016-10-13 20:50:30 +0200

    remove unused alias

--- profile.d/alias.sh
+++ profile.d/alias.sh
@@ -3,6 +3,5 @@
 alias ll='ls -lart'
 alias zshconfig='gvim ~/.zshrc'
 alias envconfig='gvim ~/env.sh'
-alias sbt_eclipse="sbt 'eclipse with-source=true'"
 alias plantuml='java -jar ~/.m2/repository/net/sourceforge/plantuml/plantuml/8018/plantuml-8018.jar'
 
