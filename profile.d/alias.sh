0215cabc6726c2a0459e42b3e62eaad9679aa639
Ander Parra <ander.pp@gmail.com>
2017-10-04 14:02:34 +0200

    add workspace alias

--- profile.d/alias.sh
+++ profile.d/alias.sh
@@ -4,4 +4,4 @@ alias ll='ls -lart'
 alias zshconfig='gvim ~/.zshrc'
 alias envconfig='gvim ~/env.sh'
 alias plantuml='java -jar ~/.m2/repository/net/sourceforge/plantuml/plantuml/8018/plantuml-8018.jar'
-
+alias ws='cd $MY_WORKSPACE'
