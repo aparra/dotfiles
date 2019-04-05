f5eb973fe3a30c568329fc8ca4fa43af8061a735
Ander Parra <ander.pp@gmail.com>
2019-04-05 13:53:57 +0200

    jenv setting - support multiple java versions

--- profile.d/env.sh
+++ profile.d/env.sh
@@ -42,3 +42,26 @@ bindkey -v
 bindkey '^R' history-incremental-search-backward
 
 source <(kubectl completion zsh)
+
+# configs from jenv
+export PATH="/Users/aparra/.jenv/shims:${PATH}"
+export JENV_SHELL=zsh
+export JENV_LOADED=1
+unset JAVA_HOME
+source '/usr/local/Cellar/jenv/0.5.2/libexec/libexec/../completions/jenv.zsh'
+jenv rehash 2>/dev/null
+jenv() {
+  typeset command
+  command="$1"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  enable-plugin|rehash|shell|shell-options)
+    eval `jenv "sh-$command" "$@"`;;
+  *)
+    command jenv "$command" "$@";;
+  esac
+}
+
