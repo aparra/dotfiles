4bc9592cd06e4d70d70c7d11e60f6b3e8577a78e
Anderson Parra <ander.pp@gmail.com>
2020-09-04 10:20:44 +0200

    support multiple java versions

--- profile.d/env.sh
+++ profile.d/env.sh
@@ -9,7 +9,11 @@ export MY_WORKSPACE=$HOME/development/workspace
 
 cdpath=($HOME $MY_WORKSPACE)
 
-export JAVA_HOME=$(/usr/libexec/java_home)
+export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
+export JAVA_14_HOME=$(/usr/libexec/java_home -v14)
+
+#default java11
+export JAVA_HOME=$JAVA_11_HOME
 
 export MAVEN_HOME=$MY_TOOLS/apache-maven-3.3.3
 export PATH=$PATH:$MAVEN_HOME/bin
@@ -39,32 +43,8 @@ if [ -f $MY_WORKSPACE/dotfiles/profile.d/private.sh ]; then
 	 . $MY_WORKSPACE/dotfiles/profile.d/private.sh
 fi
 
-eval "$(rbenv init -)"
-
 bindkey -v
 bindkey '^R' history-incremental-search-backward
 
 source <(kubectl completion zsh)
-
-# configs from jenv
-export PATH="/Users/aparra/.jenv/shims:${PATH}"
-export JENV_SHELL=zsh
-export JENV_LOADED=1
-unset JAVA_HOME
-source '/usr/local/Cellar/jenv/0.5.2/libexec/libexec/../completions/jenv.zsh'
-jenv rehash 2>/dev/null
-jenv() {
-  typeset command
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
-
-  case "$command" in
-  enable-plugin|rehash|shell|shell-options)
-    eval `jenv "sh-$command" "$@"`;;
-  *)
-    command jenv "$command" "$@";;
-  esac
-}
 
